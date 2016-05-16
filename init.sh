8dc99e070d5770c3476421b5d568f79ba390d405
redd08 <redd08@mail.ru>
2016-05-16 15:21:16 +0300

    test

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#!/bin/bash
 
-sudo /etc/init.d/gunicorn restart﻿﻿
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+# Nginx
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
