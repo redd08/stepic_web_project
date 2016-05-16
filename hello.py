d055ee07a131a014b8d123cc798f37bb674f9742
redd08 <redd08@mail.ru>
2016-05-16 14:25:54 +0300

    hello app

--- hello.py
+++ hello.py
@@ -1,16 +1,14 @@
-from urlparse import parse_qs
+import urlparse
 
-def application(environ, start_response):
-    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-    body = []
-    for key, value in query.items():
-        for item in value:
-            body.append(key + "=" + item + "\r\n")
-
+def app(environ, start_response):
+    query = urlparse.urlparse(environ['QUERY_STRING'])[4]
+    query_items = query.split("&")
     status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
+    response_headers = [
+        ('Content-type', 'text/plain')
     ]
-
-    start_response(status, headers)
-    return body
+    body = ''
+    for i in query_items:
+        body += i + '\r\n'
+    start_response(status, response_headers)
+    return [body]
--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+# Nginx
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-
-sudo /etc/init.d/gunicorn restart﻿﻿
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
