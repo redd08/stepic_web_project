29d96fd30c62695ae22e36577cb3c0820c86285a
redd08 <redd08@mail.ru>
2016-05-16 13:07:31 +0300

    ok commit

--- init.sh
+++ init.sh
@@ -2,6 +2,3 @@
 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd web
-sudo /etc/init.d/gunicorn -b 0.0.0.0:8080 hello:hello_app restart
-cd ..
